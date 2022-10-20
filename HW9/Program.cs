d14f5a6903a63d34a605bbbb0b0ee300c00720ab
unknown <saule.tapenova@mail.ru>
2022-10-20 18:03:28 +0600

    HW9

---
+++ HW9/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+int CountNumbers (int size)
+{
+    
+    int count = 0;
+    for(int i = 0; i < size; i ++) 
+    {
+        Console.WriteLine("Input number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        if(number > 0)
+        {
+            count++;
+        }
+    }        
+    return count;
+}
+Console.WriteLine("Input size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int count = CountNumbers(size);
+Console.WriteLine("Count of positive numbers:" + count);
+*/
+/*
+void FindPointInter (double b1, double b2, double k1, double k2)
+{
+    double x1;
+    x1 = (b2 - b1)/(k1 -k2);
+    double y1;
+    y1 = (k1*x1 + b1);
+    Console.Write(x1 +  ";" + y1);
+
+}
+
+FindPointInter(2,4,5,9);
+*/
